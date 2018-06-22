ad5d5ddcbc253e6481802d2827e5012fd0518aca
Charlie Burrows <charlie.burrows@gmail.com>
2018-06-22 00:56:15 -0400

    Particles flow with noise

--- src/my_sketch/core.clj
+++ src/my_sketch/core.clj
@@ -2,6 +2,8 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
+(set! *warn-on-reflection* true)
+
 (defn setup []
   ; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
@@ -12,6 +14,10 @@
   )
 
 (defn to-xy [i w] {:x (rem i w) :y (quot i w)})
+(defn from-xy [x y w] (+ x (* y w)))
+(defn wrap [x w] (cond (>= x w) 0 
+                       (< x 0) (- w 1)
+                       :else x))
 (defn update-state [state]
   (let [x 50
         y 50
@@ -20,7 +26,8 @@
         rotscl 0;.0314
         noise_scl 0.2
         regen-noise false
-        regen-field true
+        regen-field false
+        nparts 10000
 
         rot (if (nil? state) 0.0 (+ (:rot state) step))
         offset (if (or (nil? state) regen-noise)
@@ -30,12 +37,23 @@
                           ))
                       (range (* x y)))
                  (:offset state))
-        parts (if (nil? state) (iterate (fn [_] {:x (q/random x) :y (q/random y)}) 100) (:parts state))
+        parts (if (or (nil? state) (not (= nparts (count (:parts state)))))
+                (repeatedly nparts (fn [] {:x (q/random (* x scl)) :y (q/random (* y scl)) :vx 0.0 :vy 0.0}))
+               (map (fn [p]
+                      (let [px (int (/ (:x p) scl))
+                            py (int (/ (:y p) scl))
+                            acc (nth (:field state) (from-xy px py x))]
+                        {:x (wrap (+ (:x p) (:vx p)) (* x scl))
+                         :y (wrap (+ (:y p) (:vy p)) (* y scl))
+                         :vx (+ (* (:vx p) 0.75) (* (:x acc) 0.25))
+                         :vy (+ (* (:vy p) 0.75) (* (:y acc) 0.25))}))
+                    (:parts state)))
         field (if (or (nil? state) regen-field)
                 (map (fn [a off] {:x (q/cos (+ off rot (* a rotscl))) 
                                   :y (q/sin (+ off rot (* a rotscl)))}) 
                  (range (* x y)) offset)
                 (:field state))
+        iterations (if (nil? state) 1 (+ (:iterations state) 1))
         ]
     {:x x
      :y y
@@ -43,21 +61,28 @@
      :rot rot
      :offset offset
      :parts parts
-     :field field}))
+     :field field
+     :iterations iterations
+     }))
 
 (defn draw-state [state]
   ; Clear the sketch by filling it with light-grey color.
-  (q/background 240)
   ; Set color.
-  (q/stroke 0 0 0 255)
-  (doseq [[fi field] (map-indexed list (:field state))]
-    (let [{i :x j :y} (to-xy fi (:x state))]
-      (q/line (* i (:scl state))
-              (* j (:scl state))
-              (* (+ i (:x field)) (:scl state))
-              (* (+ j (:y field)) (:scl state)))))
-  (println (q/current-frame-rate))
-  )
+  (if (and (not (nil? state)) (zero? (mod (:iterations state) 10)))
+    (do
+      (q/background 240)
+      ;  (q/stroke-weight 1)
+      ;  (q/stroke 0 0 0 255)
+      ;  (doseq [[fi field] (map-indexed list (:field state))]
+      ;    (let [{i :x j :y} (to-xy fi (:x state))]
+      ;      (q/line (* i (:scl state))
+      ;              (* j (:scl state))
+      ;              (* (+ i (:x field)) (:scl state))
+      ;              (* (+ j (:y field)) (:scl state)))))
+      (q/stroke-weight 5)
+      (q/stroke 128 0 0 255)
+      (doseq [p (:parts state)] (q/point (:x p) (:y p)))
+      )))
 
 (defn -main [& args]
   (q/defsketch my-sketch

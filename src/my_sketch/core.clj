f60a08e52019870e2a88db22bacbf1af401038f6
Charlie Burrows <charlie.burrows@asml.com>
2018-06-22 13:25:14 -0400

    Wrap particles

--- src/my_sketch/core.clj
+++ src/my_sketch/core.clj
@@ -15,43 +15,53 @@
 
 (defn to-xy [i w] {:x (rem i w) :y (quot i w)})
 (defn from-xy [x y w] (+ x (* y w)))
-(defn wrap [x w] (cond (>= x w) 0 
-                       (< x 0) (- w 1)
-                       :else x))
+(defn wrap [x w] 
+  (cond (>= x w) 0.0 
+        (< x 0.0) (- w 1)
+        :else x))
+(defn hits-edge [x y w h]
+  (or (>= x w) (< x 0.0)
+      (>= y h) (< y 0.0)))
+(defn new-part [w h]
+  {:x (q/random w) :y (q/random h) :vx 0.0 :vy 0.0})
 (defn update-state [state]
-  (let [x 50
-        y 50
+  (let [x 20
+        y 20
+        scl 20
+        sx (* x scl)
+        sy (* y scl)
+
         step 0.01
-        scl 10
         rotscl 0;.0314
         noise_scl 0.2
         regen-noise false
         regen-field false
-        nparts 10000
+        nparts 1500
 
         rot (if (nil? state) 0.0 (+ (:rot state) step))
         offset (if (or (nil? state) regen-noise)
                  (map (fn [i] 
                         (let [{i :x j :y} (to-xy i x)]
-                          (* 6.28 (q/noise (* noise_scl i)  (* noise_scl j)))
-                          ))
+                          (* 6.28 (q/noise (* noise_scl i)  (* noise_scl j)))))
                       (range (* x y)))
                  (:offset state))
         parts (if (or (nil? state) (not (= nparts (count (:parts state)))))
-                (repeatedly nparts (fn [] {:x (q/random (* x scl)) :y (q/random (* y scl)) :vx 0.0 :vy 0.0}))
-               (map (fn [p]
-                      (let [px (int (/ (:x p) scl))
-                            py (int (/ (:y p) scl))
-                            acc (nth (:field state) (from-xy px py x))]
-                        {:x (wrap (+ (:x p) (:vx p)) (* x scl))
-                         :y (wrap (+ (:y p) (:vy p)) (* y scl))
-                         :vx (+ (* (:vx p) 0.75) (* (:x acc) 0.25))
-                         :vy (+ (* (:vy p) 0.75) (* (:y acc) 0.25))}))
-                    (:parts state)))
+                (repeatedly nparts (fn [] (new-part sx sy)))
+                (map (fn [p]
+                       (if (hits-edge (:x p) (:y p) sx sy)
+                         (new-part sx sy)
+                       (let [px (int (/ (:x p) scl))
+                             py (int (/ (:y p) scl))
+                             acc (nth (:field state) (from-xy px py x))]
+                         {:x (+ (:x p) (:vx p))
+                          :y (+ (:y p) (:vy p))
+                          :vx (+ (* (:vx p) 0.75) (* (:x acc) 0.25))
+                          :vy (+ (* (:vy p) 0.75) (* (:y acc) 0.25))})))
+                     (:parts state)))
         field (if (or (nil? state) regen-field)
                 (map (fn [a off] {:x (q/cos (+ off rot (* a rotscl))) 
                                   :y (q/sin (+ off rot (* a rotscl)))}) 
-                 (range (* x y)) offset)
+                     (range (* x y)) offset)
                 (:field state))
         iterations (if (nil? state) 1 (+ (:iterations state) 1))
         ]
@@ -68,21 +78,22 @@
 (defn draw-state [state]
   ; Clear the sketch by filling it with light-grey color.
   ; Set color.
-  (if (and (not (nil? state)) (zero? (mod (:iterations state) 10)))
+  (if (and (not (nil? state)) (zero? (mod (:iterations state) 1)))
     (do
       (q/background 240)
-      ;  (q/stroke-weight 1)
-      ;  (q/stroke 0 0 0 255)
-      ;  (doseq [[fi field] (map-indexed list (:field state))]
-      ;    (let [{i :x j :y} (to-xy fi (:x state))]
-      ;      (q/line (* i (:scl state))
-      ;              (* j (:scl state))
-      ;              (* (+ i (:x field)) (:scl state))
-      ;              (* (+ j (:y field)) (:scl state)))))
+      (q/stroke-weight 1)
+      (q/stroke 0 0 0 255)
+      (doseq [[fi field] (map-indexed list (:field state))]
+        (let [{i :x j :y} (to-xy fi (:x state))]
+          (q/line (* i (:scl state))
+                  (* j (:scl state))
+                  (* (+ i (:x field)) (:scl state))
+                  (* (+ j (:y field)) (:scl state)))))
       (q/stroke-weight 5)
       (q/stroke 128 0 0 255)
       (doseq [p (:parts state)] (q/point (:x p) (:y p)))
-      )))
+      ))
+  (println (q/current-frame-rate)))
 
 (defn -main [& args]
   (q/defsketch my-sketch

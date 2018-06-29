451bef02deb8d5e360d73ade8176d09b6c7f2961
Charlie Burrows <charlie.burrows@asml.com>
2018-06-29 16:06:39 -0400

    Refactor for parameterization of images

--- src/my_sketch/core.clj
+++ src/my_sketch/core.clj
@@ -4,14 +4,6 @@
 
 (set! *warn-on-reflection* true)
 
-(defn setup []
-  ; Set frame rate to 30 frames per second.
-  (q/frame-rate 120)
-  ; Set color mode to HSB (HSV) instead of default RGB.
-  (q/color-mode :hsb 1.0)
-  ; setup function returns initial state. It contains
-  ; circle color and position.
-  )
 
 (defn to-xy [i w] {:x (rem i w) :y (quot i w)})
 (defn from-xy [x y w] (+ x (* y w)))
@@ -110,6 +102,17 @@
     (or (hits-edge (:x p) (:y p) w h)
         (> (count (:history p)) max-len))))
 
+(defn call-map [i j state]
+  (apply (get-in state '(:parameters :map-fn))
+         (concat (list i j) 
+                 (get-in state '(:parameters :map-fn-parameters)))))
+
+;  :contour {:half-look 2.0
+;              :step 1.0
+;              :nsamples 50
+;              :allow-multiples-of 0.5
+;              :max-error 0.01
+   
 (defn update-contour-particles-state [state]
   (let [x (:x state)
         y (:y state)
@@ -117,24 +120,30 @@
         sx (* x scl)
         sy (* y scl)
 
-        scaled-fn (fn [i j] ((:map-fn state)
-                             (/ i scl)
-                             (/ j scl)))
-        new-part-target (fn [] (let [p (new-part sx sy)]
-                                 (assoc p :target (scaled-fn (:x p) (:y p)))))
-        nparts 100
+        scaled-fn (fn [i j] (call-map (/ i scl) (/ j scl) state))
+        new-part-target 
+        (fn [] (let [p (new-part sx sy)]
+                 (assoc p :target 
+                        (let [v (scaled-fn (:x p) (:y p))]
+                          (- v (mod v (get-in state '(:parameters :contour :allow-multiples-of))))))))
         ]
     (assoc state
-           :parts (if (or (not (contains? state :parts)) (not (= nparts (count (:parts state)))))
-                    (repeatedly nparts new-part-target)
-                    (map (fn [p]
-                           (if (is-particle-dead p sx sy)
-                             (new-part-target)
-                               (assoc (find-contour p scaled-fn (:target p))
-                                    :history (if (zero? (mod (:iterations state) 2))
-                                               (conj (:history p) {:x (:x p) :y (:y p)})
-                                               (:history p)))))
-                         (:parts state)))
+           :parts
+           (if (empty? (state :parts))
+             (repeatedly (:nparts state) new-part-target)
+             (map (fn [p]
+                    (if (is-particle-dead p sx sy)
+                      (new-part-target)
+                      (assoc (find-contour p scaled-fn (:target p))
+                             :history (if (and (zero? (mod (:iterations state)
+                                                           (get-in state '(:parameters :history-rate))))
+                                               (< (Math/abs (float
+                                                              (- (scaled-fn (:x p) (:y p))
+                                                                 (:target p))))
+                                                  (get-in state '(:parameters :contour :max-error))))
+                                        (conj (:history p) {:x (:x p) :y (:y p)})
+                                        (:history p)))))
+                  (:parts state)))
            )))
 
 (defn update-flow-particles-state [state]
@@ -144,20 +153,18 @@
         sx (* x scl)
         sy (* y scl)
 
-        nparts 200
-
         field (if (contains? state :field)
                 (:field state)
                 (map (fn [i]
-                        (let [{i :x j :y} (to-xy i x)
-                              value ((:map-fn state) i j)]
-                          {:x (q/cos value) 
-                           :y (q/sin value)}))
+                       (let [{i :x j :y} (to-xy i x)
+                             value (call-map i j state)]
+                         {:x (q/cos value) 
+                          :y (q/sin value)}))
                      (range (* x y))))
         ]
     (assoc state
-           :parts (if (or (not (contains? state :parts)) (not (= nparts (count (:parts state)))))
-                    (repeatedly nparts (fn [] (new-part sx sy)))
+           :parts (if (empty? (:parts state))
+                    (repeatedly (:nparts state) (fn [] (new-part sx sy)))
                     (map (fn [p]
                            (if (is-particle-dead p sx sy)
                              (new-part sx sy)
@@ -168,59 +175,70 @@
                                 :y (+ (:y p) (:vy p))
                                 :vx (+ (* (:vx p) 0.75) (* (:x acc) 0.25))
                                 :vy (+ (* (:vy p) 0.75) (* (:y acc) 0.25))
-                                :history (if (zero? (mod (:iterations state) 10))
+                                :history (if (zero? (mod (:iterations state) 
+                                                         (get-in state '(:parameters :history-rate))))
                                            (conj (:history p) {:x (:x p) :y (:y p)})
                                            (:history p))})))
                          (:parts state)))
            :field field)))
 
+(defn setup [parameters]
+  ; Set frame rate to 30 frames per second.
+  (q/frame-rate 120)
+  ; Set color mode to HSB (HSV) instead of default RGB.
+  (q/color-mode :hsb 1.0)
+  ; setup function returns initial state.
+  {
+   :x 50
+   :y 50
+   :scl 10
+   :nparts 200
+   :dead-parts '()
+   :iterations 0
+   :parameters parameters
+   :parts '()
+   }
+  )
+
 (defn update-state [state]
-  (let [x 50
-        y 50
-        scl 10 
-        sx (* x scl)
-        sy (* y scl)
+  (let [sx (* (:x state) (:scl state))
+        sy (* (:y state) (:scl state))
 
         map-fn (fn [i j] 
                  ;(distance i j)
                  ;(gradient i j)
                  ;(+ (* 20 (circle i j)) (sin-sin-sq i j))
                  ;(+ (* 1 (circle i j)) (sin-sin i j))
-                 (sin-sin-sq i j)
+                 ;(sin-sin-sq i j)
                  ;(+ (sin-sin-sq i j) (* 10. (pure-perlin i j)))
                  ;(* (sin-sin i j) (circle i j))
                  ;(* (pure-perlin i j) (circle i j))
                  ;(/ (circle i j) (+ i 1))
-                 ;(sin-sin i j)
+                 (sin-sin i j)
                  ;(sin-sin2 i j)
                  ;(pure-perlin i j)
                  ;(sinxsin i j)
                  ;(sinxsin-perlin i j)
                  )
 
-        dead-parts (if (nil? state) '()
-                     (let [new-dead (filter (complement empty?) 
-                                            (map :history (filter (fn [p] (is-particle-dead p sx sy)) (:parts state))))]
-                       (if (nil? new-dead)
-                         (:dead-parts state)
-                         (concat new-dead (:dead-parts state))
-                           )))
-
-        iterations (if (nil? state) 1 (+ (:iterations state) 1))
         ]
     (assoc state
-           :x x
-           :y y
-           :scl scl
-           :map-fn map-fn
-           :dead-parts dead-parts
-           :iterations iterations
-           )))
+           :dead-parts
+           (let [new-dead (filter (complement empty?) 
+                                  (map :history (filter (fn [p] (is-particle-dead p sx sy))
+                                                        (:parts state))))]
+             (if (nil? new-dead)
+               (:dead-parts state)
+               (concat new-dead (:dead-parts state))))
+           
+           :iterations (+ (:iterations state) 1))))
 
 (defn draw-state [state]
   ; Clear the sketch by filling it with light-grey color.
   ; Set color.
-  (if (and (not (nil? state)) (zero? (mod (:iterations state) 100)))
+  (println 'draw (:iterations state))
+  (if (and (not (empty? (:parts state)))
+           (zero? (mod (:iterations state) 100)))
     (do
       (println (q/current-frame-rate))
       (q/background 0.8)
@@ -239,9 +257,10 @@
                     (* (+ j (:y field)) (:scl state))))))
 
       (q/stroke 0.5 0 0 0.2)
-      (let [values (map (fn [p] ((:map-fn state) (:x p) (:y p)))
-                       (concat (:parts state)
-                               (map first (:dead-parts state))))
+
+      (let [values (map (fn [p] (call-map (:x p) (:y p) state))
+                        (concat (:parts state)
+                                (map first (:dead-parts state))))
             hue1 0.1
             hue2 0.9
             high (apply max values)
@@ -250,7 +269,7 @@
         (when true
           ;(q/stroke-weight 5)
           (doseq [p (:parts state)]
-            (q/stroke (fac ((:map-fn state) (:x p) (:y p))) 1.0 0.8 0.2)
+            (q/stroke (fac (call-map (:x p) (:y p) state)) 1.0 0.8 0.2)
             ;(q/point (:x p) (:y p))
             (doseq [[h hp] (map list (:history p) (rest (:history p)))]
               (q/line (:x h) (:y h) (:x hp) (:y hp)))
@@ -258,29 +277,64 @@
 
         (when true ; Dead part lines
           (doseq [p (:dead-parts state)]
-            (q/stroke (fac ((:map-fn state) (:x (first p)) (:y (first p)))) 1.0 0.8 0.2)
+            (q/stroke (fac (call-map (:x (first p)) (:y (first p)) state)) 1.0 0.8 0.2)
             (doseq [[h hp] (map list p (rest p))]
-              (q/line (:x h) (:y h) (:x hp) (:y hp)))))))
-    )
+              (q/line (:x h) (:y h) (:x hp) (:y hp))))))))
 
-
-  (when (and true (not (nil? state)) (zero? (mod (:iterations state) 30)))
+  (when (and false (not (nil? state)) (zero? (mod (:iterations state) 30)))
     (q/save "hairy.png"))
 
   )
 
+
+(def parameters 
+  {
+   :sin-sin-flow
+   {:map-fn sin-sin
+    :map-fn-parameters '()
+    :update-type :flow
+    :history-rate 10
+    }
+
+   :sin-sin-contours-stepped
+   {:map-fn sin-sin
+    :map-fn-parameters '()
+    :update-type :contour
+    :contour {:half-look 2.0
+              :step 1.0
+              :nsamples 50
+              :allow-multiples-of 0.5
+              :max-error 0.01
+              }
+    :history-rate 2
+    }
+
+   :template
+   {:map-fn identity
+    :map-fn-parameters '()
+    :update-type :flow
+    :contour {:half-look 3.1415
+              :step 1.
+              :nsamples 50
+              :allow-multiples-of 2
+              :max-error 0.01
+              }
+    :history-rate 10
+    }}
+  )
 (defn -main [& args]
   (q/defsketch my-sketch
     :title "You spin my circle right round"
     :size [500 500]
     ; setup function called only once, during sketch initialization.
-    :setup setup
+    :setup (fn [] (setup (:sin-sin-contours-stepped parameters)))
     ; update-state is called on each iteration before draw-state.
     :update (fn [state] (-> state
                             update-state
-                            ;update-flow-particles-state
-                            update-contour-particles-state
-                            ))
+                            ((fn [state]
+                               (case (get-in state '(:parameters :update-type))
+                                 :flow (update-flow-particles-state state)
+                                 :contour (update-contour-particles-state state))))))
     :draw draw-state
     :features [:keep-on-top]
     ; This sketch uses functional-mode middleware.
